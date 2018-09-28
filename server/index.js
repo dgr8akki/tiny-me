88e67bb9cb2e5d3eb9e0e94492c62b16f6a20f86
Aakash Pahuja <aakash.pahuja@kony.com>
2018-09-29 03:09:43 +0530

    Rewamp

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ routes(app);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
   });
 }
 app.listen(port, () => console.log(`Listening on port ${port}`));
