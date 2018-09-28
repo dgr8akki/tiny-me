512fdd41a4d351843c8abcfcb2757120d70420e9
Aakash Pahuja <aakash.pahuja@kony.com>
2018-09-29 02:13:46 +0530

    Add initial setup

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("../routes/index.js");
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+routes(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+app.listen(port, () => console.log(`Listening on port ${port}`));
