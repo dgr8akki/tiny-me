562623f3e23851660b8feacdde3a2700d6da3460
Aakash Pahuja <aakash.pahuja@kony.com>
2018-09-29 16:45:23 +0530

    Still configuring heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {
-    response: ''
-  };
+  // Initialize state
+  state = { passwords: [], message: '' }
 
+  // Fetch passwords after first mount
   componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({
-        response: res.message
-      }))
-      .catch(er => console.log(er));
+    this.getPasswords();
+    this.getMessage();
   }
 
-  callApi = async () => {
-    const response = await fetch('/api/test');
-    const body = await response.json();
+  getPasswords = () => {
+    // Get the passwords and store them in state
+    fetch('/api/passwords')
+      .then(res => res.json())
+      .then(passwords => this.setState({ passwords }));
+  }
 
-    if (response.status !== 200) throw Error(body.message);
+  getMessage = () => {
+  // Get the passwords and store them in state
+  fetch('/api/test')
+    .then(res => res.json())
+    .then(message => this.setState({ message }));
+  }
 
-    return body;
-  };
   render() {
-    console.log(this.state)
+    const { passwords, message } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-        {
-          this.state.response
-        }
-        </p>
+        {/* Render the passwords if we have them */}
+        {passwords.length ? (
+          <div>
+            <h1>4 Passwords.</h1>
+            <ul className="passwords">
+              {/*
+                Generally it's bad to use "index" as a key.
+                It's ok for this example because there will always
+                be the same number of passwords, and they never
+                change positions in the array.
+              */}
+              {passwords.map((password, index) =>
+                <li key={index}>
+                  {password}
+                </li>
+              )}
+            </ul>
+            <button
+              className="more"
+              onClick={this.getPasswords}>
+              {message || "Get MORE"}
+            </button>
+          </div>
+        ) : (
+          // Render a helpful message otherwise
+          <div>
+            <h1>No passwords :(</h1>
+            <button
+              className="more"
+              onClick={this.getPasswords}>
+              Try Again?
+            </button>
+          </div>
+        )}
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -4,19 +4,18 @@ const routes = require("../routes/index.js");
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 5000;
-const dev = app.get('env') !== 'production';
+// const dev = app.get('env') !== 'production';
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
 
 routes(app);
 
-if (!dev) {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });
-}
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
